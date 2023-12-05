5f7c51ddbd6de56b62f82f6b59d8ba5984880a1f
Jacob Lorenzen <jacob.lorenzen@velux.com>
2023-12-05 10:16:01 +0100

    chore: solution for day05a

--- lib/day05.rb
+++ lib/day05.rb
@@ -1,11 +1,99 @@
 # frozen_string_literal: true
 
-class Day05
-  def load(file)
-    @lines = File.readlines(file)
+# Stores agricultral data
+class Almanac
+  def initialize(destinations, sources, ranges)
+    @sources = sources
+    @destinations = destinations
+    @ranges = ranges
   end
 
-  def part1
-    0
+  def get_seed(seed)
+    result = seed
+    @sources.each_with_index do |source, index|
+      if source + @ranges[index] > seed && source <= seed
+        offset = seed - source
+        result = @destinations[index] + offset
+      end
+    end
+    result
+  end
+
+  def to_s
+    "Almanac #{@destination} #{@source} #{@range}"
+  end
+end
+
+class Day05
+  def initialize
+    @seed = nil
+    @soil = nil
+    @fertilizer = nil
+    @water = nil
+    @light = nil
+    @temperature = nil
+    @humidity = nil
+    @seeds = []
+  end
+
+  def parse_section(file, section)
+    source = []
+    destination = []
+    range = []
+    line = file.readline
+    if line.start_with? section
+      line = file.readline
+      until line.empty?
+        input = line.split(' ').map(&:to_i)
+        destination << input[0]
+        source << input[1]
+        range << input[2]
+
+        break if file.eof?
+
+        line = file.readline.chomp
+      end
+    end
+    [destination, source, range]
+  end
+
+  def parse(filename)
+    File.open(filename, 'r') do |file|
+      line = file.readline
+      @seeds = line.split(':')[1].split(' ').map(&:to_i) if line.start_with? 'seeds'
+      file.readline # skip blank line
+
+      sections = parse_section(file, 'seed-to-soil map')
+      @seed = Almanac.new(sections[0], sections[1], sections[2])
+      sections = parse_section(file, 'soil-to-fertilizer map')
+      @soil = Almanac.new(sections[0], sections[1], sections[2])
+      sections = parse_section(file, 'fertilizer-to-water map')
+      @fertilizer = Almanac.new(sections[0], sections[1], sections[2])
+      sections = parse_section(file, 'water-to-light map')
+      @water = Almanac.new(sections[0], sections[1], sections[2])
+      sections = parse_section(file, 'light-to-temperature map')
+      @light = Almanac.new(sections[0], sections[1], sections[2])
+      sections = parse_section(file, 'temperature-to-humidity map')
+      @temperature = Almanac.new(sections[0], sections[1], sections[2])
+      sections = parse_section(file, 'humidity-to-location map')
+      @humidity = Almanac.new(sections[0], sections[1], sections[2])
+    end
+  end
+
+  def part1(filename)
+    parse(filename)
+    locations = []
+
+    @seeds.each do |seed|
+      soil = @seed.get_seed(seed)
+      fertilizer = @soil.get_seed(soil)
+      water = @fertilizer.get_seed(fertilizer)
+      light = @water.get_seed(water)
+      temperature = @light.get_seed(light)
+      humidity = @temperature.get_seed(temperature)
+      locations << @humidity.get_seed(humidity)
+    end
+
+    locations.min
   end
 end
--- test/unit/day05_test.rb
+++ test/unit/day05_test.rb
@@ -3,10 +3,14 @@
 require 'test_helper'
 require 'day05'
 
-class Day01Test < Minitest::Test
+class Day05Test < Minitest::Test
   def test_day05a
     sut = Day05.new
-    sut.load('data/day05a.txt')
-    assert_equal(35, sut.part1)
+    assert_equal(35, sut.part1('data/day05a.txt'))
+  end
+
+  def test_day05_part1
+    sut = Day05.new
+    assert_equal(382_895_070, sut.part1('data/day05.txt'))
   end
 end

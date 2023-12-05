31b72598388db8ab3bd39aa851db6f087608b85b
Jacob Lorenzen <jacob.lorenzen@velux.com>
2023-12-05 08:26:52 +0100

    chore: solution for day04b

--- lib/day04.rb
+++ lib/day04.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Day04
   def load(file)
     @lines = File.readlines(file)
@@ -14,5 +16,28 @@ class Day04
       sum += (matches - 1).times.reduce(1) { |acc, _| acc * 2 } if matches.positive?
     end
     sum
+  end
+
+  def part2
+    cards = {}
+    @lines.each do |line|
+      segments = line.split(':')
+      card = segments[0].split(' ')[1].to_i
+      numbers = segments[1].split('|')
+      drawn = numbers[0].split(' ').map(&:to_i)
+      winners = numbers[1].split(' ').map(&:to_i)
+      matches = (Set.new(drawn) & Set.new(winners)).size
+      cards[card] = matches
+    end
+
+    iterations = 0
+    queue = cards.map { |k, _| k }
+    max = cards.max_by { |k, _| k }[0]
+    while queue.size.positive?
+      iterations += 1
+      card = queue.shift
+      (1..cards[card]).filter { |i| i <= max }.map { |i| card + i }.each { |c| queue << c } if cards[card].positive?
+    end
+    iterations
   end
 end
--- test/unit/day04_test.rb
+++ test/unit/day04_test.rb
@@ -1,16 +1,29 @@
-require "test_helper"
-require "day04"
+# frozen_string_literal: true
+
+require 'day04'
 
 class Day01Test < Minitest::Test
   def test_day04a
     sut = Day04.new
-    sut.load("data/day04a.txt")
+    sut.load('data/day04a.txt')
     assert_equal(13, sut.part1)
   end
 
-  def test_day04
+  def test_day04b
     sut = Day04.new
-    sut.load("data/day04.txt")
-    assert_equal(20107, sut.part1)
+    sut.load('data/day04a.txt')
+    assert_equal(30, sut.part2)
+  end
+
+  def test_day04_part1
+    sut = Day04.new
+    sut.load('data/day04.txt')
+    assert_equal(20_107, sut.part1)
+  end
+
+  def test_day04_part2
+    sut = Day04.new
+    sut.load('data/day04.txt')
+    assert_equal(8_172_507, sut.part2)
   end
 end

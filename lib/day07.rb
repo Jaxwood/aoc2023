5b2af7e953b5316a8016c65051ec4704a786b5f6
Jacob Lorenzen <jacob.lorenzen@velux.com>
2023-12-07 13:14:45 +0100

    chore: solution for day07b

--- lib/day07.rb
+++ lib/day07.rb
@@ -12,11 +12,19 @@ end
 
 # Camel Hand
 class CamelHand
-  def initialize(raw, score)
+  def initialize(raw, score, joker: false)
     @cards = raw.chars
     @score = score.to_i
-    @hand_type = determine_hand_type
-    order = %w[A K Q J T 9 8 7 6 5 4 3 2]
+    @hand_type = if joker
+                   determine_hand_type_with_joker
+                 else
+                   determine_hand_type
+                 end
+    order = if joker
+              %w[A K Q T 9 8 7 6 5 4 3 2 J]
+            else
+              %w[A K Q J T 9 8 7 6 5 4 3 2]
+            end
     @rank = []
     @cards.each { |card| @rank << order.index(card) }
   end
@@ -46,6 +54,23 @@ class CamelHand
 
     panic 'Unknown hand type'
   end
+
+  def determine_hand_type_with_joker
+    return determine_hand_type unless @cards.include?('J')
+
+    jokers = @cards.filter { |card| card == 'J' }.count
+    type = @cards.reject { |card| card == 'J' }.tally.values.sort
+
+    return HandType::FIVE_OF_A_KIND if type == [5 - jokers] || jokers == 5
+    return HandType::FOUR_OF_A_KIND if type == [1, 4 - jokers]
+    return HandType::FULL_HOUSE if type == [2, 3 - jokers]
+    return HandType::THREE_OF_A_KIND if type == [1, 1, 3 - jokers]
+    return HandType::TWO_PAIR if type == [1, 2, 2 - jokers]
+    return HandType::ONE_PAIR if type == [1, 1, 1, 2 - jokers]
+    return HandType::HEIGH_CARD if type == [1, 1, 1, 1, 1]
+
+    panic 'Unknown hand type'
+  end
 end
 
 # Day07
@@ -59,6 +84,17 @@ class Day07
     @lines.each do |line|
       parts = line.split(' ')
       hands << CamelHand.new(parts[0], parts[1])
+    end
+
+    hands.sort_by! { |hand| [hand.hand_type, hand.rank] }
+    hands.reverse.each_with_index.reduce(0) { |total, (hand, index)| total + hand.score(index + 1) }
+  end
+
+  def part2
+    hands = []
+    @lines.each do |line|
+      parts = line.split(' ')
+      hands << CamelHand.new(parts[0], parts[1], joker: true)
     end
 
     hands.sort_by! { |hand| [hand.hand_type, hand.rank] }
--- test/unit/day07_test.rb
+++ test/unit/day07_test.rb
@@ -10,9 +10,21 @@ class Day07Test < Minitest::Test
     assert_equal(6440, sut.part1)
   end
 
+  def test_day07b
+    sut = Day07.new
+    sut.load('data/day07a.txt')
+    assert_equal(5905, sut.part2)
+  end
+
   def test_day07_part1
     sut = Day07.new
     sut.load('data/day07.txt')
     assert_equal(248_179_786, sut.part1)
+  end
+
+  def test_day07_part2
+    sut = Day07.new
+    sut.load('data/day07.txt')
+    assert_equal(247_885_995, sut.part2)
   end
 end

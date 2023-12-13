1ac8a1114bc7e4a7073ebde3888dfcfc894b6b8b
Jacob Lorenzen <jacob.lorenzen@velux.com>
2023-12-13 11:05:48 +0100

    chore: solution for day12b

--- lib/day12.rb
+++ lib/day12.rb
@@ -2,31 +2,37 @@
 
 # Day12
 class Day12
+  def initialize
+    @cache = {}
+  end
+
   def arrange(row, criteria)
-    if criteria.empty?
-      return 1 unless row.include?('#')
+    @cache["#{row}-#{criteria}"] ||= begin
+      if criteria.empty?
+        return 1 unless row.include?('#')
 
-      return 0
+        return 0
+      end
+
+      return 0 if row.empty?
+
+      next_character = row.first
+      next_criteria = criteria.first
+      result = 0
+
+      case next_character
+      when '#'
+        result = pound(row, criteria, next_criteria)
+      when '.'
+        result = dot(row, criteria)
+      when '?'
+        result = dot(row, criteria) + pound(row, criteria, next_criteria)
+      else
+        raise "Unknown character #{next_character}"
+      end
+
+      result
     end
-
-    return 0 if row.empty?
-
-    next_character = row.first
-    next_criteria = criteria.first
-    result = 0
-
-    case next_character
-    when '#'
-      result = pound(row, criteria, next_criteria)
-    when '.'
-      result = dot(row, criteria)
-    when '?'
-      result = dot(row, criteria) + pound(row, criteria, next_criteria)
-    else
-      raise "Unknown character #{next_character}"
-    end
-
-    result
   end
 
   def pound(row, criteria, criterion)
@@ -67,6 +73,14 @@ class Day12
     sum = 0
     parse.map do |spring|
       sum += arrange(spring[0].chars, spring[1])
+    end
+    sum
+  end
+
+  def part2
+    sum = 0
+    parse.map do |spring|
+      sum += arrange(("#{spring[0]}?" * 5).chop.chars, spring[1] * 5)
     end
     sum
   end
--- test/unit/day12_test.rb
+++ test/unit/day12_test.rb
@@ -8,9 +8,21 @@ class Day12Test < Minitest::Test
     assert_equal(21, sut.part1)
   end
 
+  def test_day12b
+    sut = Day12.new
+    sut.load('data/day12a.txt')
+    assert_equal(525_152, sut.part2)
+  end
+
   def test_day12_part1
     sut = Day12.new
     sut.load('data/day12.txt')
     assert_equal(7047, sut.part1)
+  end
+
+  def test_day12_part2
+    sut = Day12.new
+    sut.load('data/day12.txt')
+    assert_equal(17_391_848_518_844, sut.part2)
   end
 end
